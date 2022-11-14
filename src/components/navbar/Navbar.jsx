0529a605e244a84797494c6c4595434cda0137ce
kenwekesa <kenwek1994@gmail.com>
2022-11-14 21:22:20 +0300

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./navbar.scss"
 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+
+
 function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <ul className="nav_menu">
+        <li className="menu_item">Home</li>
+        <li className="menu_item">Products</li>
+        <li className="menu_item">Users</li>
+        <li className="menu_item">Orders</li>
+        
+      </ul>
+      <div className="user">
+          <AccountCircleIcon />
+        </div>
+    </div>
   )
 }
 
